b7f7e84d0402bdda7dedf60d372e9376b6c857ca
natecheadle <92184613+natecheadle@users.noreply.github.com>
2021-10-28 16:23:45 -0700

    Add CMakePresets File (#2)
    * Added CMakePresets to Repo

--- .github/workflows/configure_ubuntu.sh
+++ .github/workflows/configure_ubuntu.sh
@@ -1,8 +1,3 @@
 apt-get install -y \
 build-essential tar zip unzip git wget \
-ninja-build python c++filt xz-utils curl
-
-wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz && \
-tar -xf clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz && \
-mv clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04 /usr/bin/llvm_13 && \
-rm clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+ninja-build python c++filt xz-utils curl clang-12
