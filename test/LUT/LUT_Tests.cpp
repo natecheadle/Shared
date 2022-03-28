7a53269866c22ef0b958470fbe37b133ffb719aa
natecheadle_school <natecheadle@users.noreply.github.com>
2022-03-27 21:57:55 -0700

    added additional tests

--- test/LUT/LUT_Tests.cpp
+++ test/LUT/LUT_Tests.cpp
@@ -5,15 +5,34 @@
 #include <string>
 
 namespace Shared {
-    constexpr std::array<std::pair<int, std::string_view>, 3> TestTable = {
+    constexpr std::array<std::pair<int, std::string_view>, 3> SimpleTestTable = {
         {{1, "one"}, {2, "two"}, {3, "three"}}
     };
-    typedef CompileTimeLookup<int, std::string_view, 3, TestTable> TestLookup;
+    typedef CompileTimeLookup<int, std::string_view, 3, SimpleTestTable> SimpleTestLookup;
+
+    enum class MyType
+    {
+        Test_Type,
+        Another_Type,
+        Last_type,
+    };
+
+    constexpr std::array<std::pair<MyType, std::string_view>, 3> ComplexTestTable = {
+        {{MyType::Test_Type, "Test Type"}, {MyType::Another_Type, "Another Type"}, {MyType::Last_type, "Last Type"}}
+    };
+    typedef CompileTimeLookup<MyType, std::string_view, 3, ComplexTestTable> ComplexTestLookup;
 
     TEST(LUT_Tests, TestBasicLookup)
     {
-        ASSERT_EQ(std::string("one"), TestLookup::GetValue(1));
-        ASSERT_EQ(std::string("two"), TestLookup::GetValue(2));
-        ASSERT_EQ(std::string("three"), TestLookup::GetValue(3));
+        ASSERT_EQ(std::string("one"), SimpleTestLookup::GetValue(1));
+        ASSERT_EQ(std::string("two"), SimpleTestLookup::GetValue(2));
+        ASSERT_EQ(std::string("three"), SimpleTestLookup::GetValue(3));
+    }
+
+    TEST(LUT_Tests, TestComplexLookup)
+    {
+        ASSERT_EQ(std::string("Test Type"), ComplexTestLookup::GetValue(MyType::Test_Type));
+        ASSERT_EQ(std::string("Another Type"), ComplexTestLookup::GetValue(MyType::Another_Type));
+        ASSERT_EQ(std::string("Last Type"), ComplexTestLookup::GetValue(MyType::Last_type));
     }
 } // namespace Shared
